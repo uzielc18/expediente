4a4512fe4585f78224b780b3ff1dab8ef6473181
Uziel <uzielc18@gmail.com>
2018-09-15 21:42:44 -0500

    limiando el static

--- default/app/controllers/apps/detallestecnicos_controller.php
+++ default/app/controllers/apps/detallestecnicos_controller.php
@@ -1,15 +1,21 @@
 <?php
 View::template('apps/default_expediente');
-class MetradosController extends AppController {
+class DetallestecnicosController extends AppController {
     
-    public function crear($partida_id){		
+    public function crear($partida_id,$id=0){		
 		$Detallestecnicos = new Detallestecnicos();
         $Partidas = new Partidas();
         $Modulos = new Modulos();
-        $this->modulo=$Modulos->find_first('conditions: codigo="ACU"');
+        $this->modulo=$Modulos->find_first('conditions: codigo="ESPTEC"');
         $this->partida=$Partidas->find((int)$partida_id);
-		$this->titulo='Analisis de costos unitarios <b>'.$this->partida->nombre.'</b> del Modulo <b>'.$this->modulo->descripcion.'</b>';
-        $this->titulo_small='Todos los detalles';
+		$this->titulo='Detalle tecnicos<b>'.$this->partida->nombre.'</b> del Modulo <b>'.$this->modulo->descripcion.'</b>';
+        $this->titulo_small='';
+        if($id==0){
+        	$detallestecnicos=$Detallestecnicos->find_first('partidas_id='.$partida_id);
+        	return Redirect::toAction('crear/'.$partida_id.'/'.$detallestecnicos->id);
+        }else{
+        	$this->detallestecnicos=$Detallestecnicos->find((int) $id);
+        }
 		if (Input::hasPost('detallestecnicos')) {
 
             $obj = new Detallestecnicos();
@@ -19,36 +25,20 @@ class MetradosController extends AppController {
                 $this->detallestecnicos = $obj;
                 return;
             }
-            $PP = new Partidas();
-            $PP->save(Input::post('partida'));
-            return Redirect::toAction('crear/'.$partida_id);   
+            //$PP = new Partidas();
+            //$PP->save(Input::post('partida'));
+            return Redirect::toAction('crear/'.$partida_id.'/'.$obj->id);   
             //return Redirect::to('apps/expediente/generar/'.$exp_id.'/'.$mod_id.'/'.$bloc_id.'/'.$pres_id);          
         }
 	}
-    public function editar($partida_id, $id){
-        View::select('crear');  
-       $Detallestecnicos = new Detallestecnicos();
-        $Partidas = new Partidas();
-        $Modulos = new Modulos();
-        $this->modulo=$Modulos->find_first('conditions: codigo="ACU"');
-        $this->partida=$Partidas->find((int)$partida_id);
-		$this->titulo='Analisis de costos unitarios <b>'.$this->partida->nombre.'</b> del Modulo <b>'.$this->modulo->descripcion.'</b>';
-        $this->titulo_small='Todos los detalles';
-        
-		if (Input::hasPost('detallestecnicos')) {
-
-            $obj = new Detallestecnicos();
-            if (!$obj->save(Input::post('detallestecnicos'))) {
-                Flash::error('Falló Operación');
-                //se hacen persistente los datos en el formulario
-                $this->detallestecnicos = $obj;
-                return;
-            }
-            $PP = new Partidas();
-            $PP->save(Input::post('partida'));
-            return Redirect::toAction('crear/'.$partida_id);   
-            //return Redirect::to('apps/expediente/generar/'.$exp_id.'/'.$mod_id.'/'.$bloc_id.'/'.$pres_id);          
+	public function eliminar($partida_id,$id) {
+        $obj = new Detallestecnicos();
+        if (!$obj->delete($id)) {
+            Flash::error('Falló Operación');
         }
+        //enrutando al index para listar los articulos
+        Flash::error('Detalle eliminado');
+        Redirect::toAction('crear/'.$partida_id.'/0');
     }
 
     public function terminar($exp_id,$mod_id,$bloc_id,$pres_id){    
@@ -57,7 +47,7 @@ class MetradosController extends AppController {
 
             $obj = new Partidas();
             $datos = Input::post('partida');
-            $datos['detallemetrados']=base64_encode($datos['detallemetrados']);
+            $datos['detallestecnicos']=$datos['detallestecnicos'];
             print_r($datos);
             if (!$obj->save($datos)) {
                 Flash::error('Falló Operación');
