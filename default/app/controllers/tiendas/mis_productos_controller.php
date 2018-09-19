238518d6d4b8f18de39f43471fae09a9df49f241
Uziel <uzielc18@gmail.com>
2018-09-19 05:41:31 -0500

    actulizacion de tiend

---
+++ default/app/controllers/tiendas/mis_productos_controller.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * 
+ */
+View::template('apps/default_app');
+class MisProductosController extends AppController
+{
+	public function index(){
+		
+	}
+    public function listar(){
+        
+    }
+}
+?>
