7908f4e9602934553ccbf2098a8657758c2028c8
Uziel Carpio <uzielc18@gmail.com>
2018-09-20 18:21:47 -0500

    actualizacion

--- default/app/controllers/tiendas/mis_productos_controller.php
+++ default/app/controllers/tiendas/mis_productos_controller.php
@@ -8,15 +8,74 @@ class MisProductosController extends AppController
 	public function index(){
 		
 	}
-    public function listar($page=1){
+    public function listar($tipomateriales_id=1,$indices_id=NULL$page=1){
+    	$this->titulo="Administracion de materiales";
+		$this->id=$tipomateriales_id;
+		$TipoMateriales = new Tipomateriales();
+		$Materiales = new Materiales();
+		$this->tipos = $TipoMateriales->get_principales();
+		$this->tipo = $TipoMateriales->find((int)$tipomateriales_id);
+		$this->indices = $TipoMateriales->find('conditions: estado=1 AND tipomateriales_id='.$tipomateriales_id);
+		$this->indice = false;
+		if($indices_id){
+			$this->indice = $TipoMateriales->find((int)$indices_id);
+			$this->materiales=$Materiales->find('conditions: estado=1 AND tipomateriales_id='.$indices_id);
+		}
     	$this->titulo='Mis productos';
         $MisProductos= new Preciosempresas();
         $per_page=10;
         $this->results=$MisProductos->getMisProductos($per_page, $page);
     }
 
-    public function crear(){
-    	$this->titulo='Crear producto';
-    }
+    	public function crear($tipomateriales_id,$indices_id)
+	{
+		$this->titulo="Crear materiales nuevos";
+		$Materiales = new Materiales();		
+		$TipoMateriales = new Tipomateriales();		
+		$this->tipo = $TipoMateriales->find((int)$tipomateriales_id);
+		$this->indices_id = $indices_id;
+		$this->codigo_new=$Materiales->get_codigo($indices_id);
+		if (Input::hasPost('materiales')) {
+
+            $obj = $Materiales;
+            //En caso que falle la operación de guardar
+            if (!$obj->save(Input::post('materiales'))) {
+                Flash::error('Falló Operación');
+                //se hacen persistente los datos en el formulario
+                $this->materiales = $obj;
+                return;
+            }
+
+            return Redirect::to('apps/materiales/index/'.$tipomateriales_id.'/'.$indices_id.'#');
+        }
+
+	}
+
+	public function editar($tipomateriales_id,$indices_id,$id)
+	{
+		View::select("crear");
+		$this->titulo="Editar Materiales";
+		$Materiales = new Materiales();		
+		$TipoMateriales = new Tipomateriales();		
+		$this->tipo = $TipoMateriales->find((int)$tipomateriales_id);
+		$this->indices_id = $indices_id;
+		if (Input::hasPost('materiales')) {
+
+            $obj = $Materiales;
+            //En caso que falle la operación de guardar
+            if (!$obj->save(Input::post('materiales'))) {
+                Flash::error('Falló Operación');
+                //se hacen persistente los datos en el formulario
+                $this->materiales = $obj;
+                return;
+            }
+
+            return Redirect::to('apps/materiales/index/'.$tipomateriales_id.'/'.$indices_id.'#');
+        }
+
+        $this->materiales = $Materiales->find((int) $id);
+		$this->codigo_new=$this->materiales->codigo;
+
+	}
 }
 ?>
